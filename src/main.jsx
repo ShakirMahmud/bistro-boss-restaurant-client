3c7cf93f0f3aa9a913622c285fda2fc0b4f5f627
ShakirMahmud <shakirmahmud50@gmail.com>
2024-12-30 20:10:11 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { router } from './routers/router'
+import { RouterProvider } from 'react-router-dom'
+// import App from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    
+      <RouterProvider router={router} />
+    
+  </StrictMode>,
+)
