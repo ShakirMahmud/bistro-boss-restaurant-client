aaffcdb757a867b351a738effd2f2480ec1b792e
ShakirMahmud <shakirmahmud50@gmail.com>
2024-12-30 21:53:38 +0600

    Setup routes, layout and footer

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className=' w-full bg-black absolute -top-1'>
+            <h2 className='text-white'>Banner</h2>
+        </div>
+    );
+};
+
+export default Banner;
