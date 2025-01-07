664956a5fca1599c236c8cdbe2fd344372f839a8
ShakirMahmud <shakirmahmud50@gmail.com>
2025-01-08 02:27:04 +0600

    Create a JWT token and save it on local storage

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,11 +1,13 @@
 import { createContext, useEffect, useState } from "react";
 import { auth } from './../firebase/firebase.config';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext();
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null); 
     const [loading, setLoading] = useState(true);
+    const axiosPublic = useAxiosPublic();
 
     // create user
     const createUser = (email, password) => {
@@ -54,6 +56,20 @@ const AuthProvider = ({children}) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('current user', currentUser);
+            if(currentUser){
+                // create token
+                const userInfo = { email: currentUser.email}
+                axiosPublic.post('/jwt', userInfo)
+                    .then(data => {
+                        if(data.data.token){
+                            // set token
+                            localStorage.setItem('token', data.data.token);
+                        }
+                    })
+            }else{
+                // remove token
+                localStorage.removeItem('token');
+            }
             setLoading(false);
         });
         return () => unsubscribe();
