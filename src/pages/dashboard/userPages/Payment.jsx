f72275f754fa2ac7fd5e0ad189cda3f36220abf6
ShakirMahmud <shakirmahmud50@gmail.com>
2025-01-12 21:31:10 +0600

    Create Stripe account and use publishable key

--- src/pages/dashboard/userPages/Payment.jsx
+++ src/pages/dashboard/userPages/Payment.jsx
@@ -1,8 +1,9 @@
 import { Elements } from "@stripe/react-stripe-js";
 import SectionTitle from "../../../components/SectionTitle";
 import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
-const stripePromise = loadStripe("pk_test_51J0QJvKXWd0r4JYQnW8z6mZcP4f5K2jvM8XoOa7K4iZQX9bOyZgj2h6r7LxT9cQxu0fJYqoT9sG7K4Ct6E8LwMm0c0z2b");
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_Key);
 const Payment = () => {
     return (
         <div>
@@ -10,7 +11,7 @@ const Payment = () => {
 
             <div>
                 <Elements stripe={stripePromise}>
-                    
+                    <CheckoutForm  /> 
                 </Elements>
             </div>
             
